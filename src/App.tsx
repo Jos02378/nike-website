56ed49ef2476501babe9b3db5856a843551c6a74
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-02-24 01:17:47 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main className='relative'>
+      Nav
+      <section className='padding-b xl:padding-1 wide:padding-3'>Hero</section>
+      <section className='padding'>popular products</section>
+      <section className='padding'>super quality</section>
+      <section className='padding-x py-10'>services</section>
+      <section className='padding'>special offer</section>
+      <section className='padding bg-pale-blue'>customer reviews</section>
+      <section className='sm:py-32 py-16 w-full padding-x'>subscribe</section>
+      <section className='bg-black padding-x padding-t pb-8'>footer</section>
+    </main>
+  );
+};
+
+export default App;
