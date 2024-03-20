ab8729283d3ed9f097b1d2c09cd48215f94cca1d
Joseph Haryanto <57058570+Jos02378@users.noreply.github.com>
2024-03-20 23:08:25 +0700

    feat: add sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,43 @@
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  SpecialOffers,
+  Services,
+  Subscribe,
+  SuperQuality,
+} from './sections';
+import Navbar from './components/Navbar';
+
 const App = () => {
   return (
     <main className='relative'>
-      Nav
-      <section className='padding-b xl:padding-1 wide:padding-3'>Hero</section>
-      <section className='padding'>popular products</section>
-      <section className='padding'>super quality</section>
-      <section className='padding-x py-10'>services</section>
-      <section className='padding'>special offer</section>
-      <section className='padding bg-pale-blue'>customer reviews</section>
-      <section className='sm:py-32 py-16 w-full padding-x'>subscribe</section>
-      <section className='bg-black padding-x padding-t pb-8'>footer</section>
+      <Navbar />
+      <section className='padding-b xl:padding-1 wide:padding-3'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffers />
+      </section>
+      <section className='padding bg-pale-blue'>
+        <CustomerReviews />
+      </section>
+      <section className='sm:py-32 py-16 w-full padding-x'>
+        <Subscribe />
+      </section>
+      <section className='bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
     </main>
   );
 };
